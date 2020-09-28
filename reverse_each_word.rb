a0de68dd890ef50c82d27fdb58b2f0cbc7b5c987
Michael Lee <michael.l33.1994@gmail.com>
2020-09-28 19:34:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(element)
-  reverse = element.split("").collect do |word|
+  reverse = element.split(" ").collect do |word|
     word.reverse
   end
-  reverse.join("")
+  reverse.join(" ")
     
 end
